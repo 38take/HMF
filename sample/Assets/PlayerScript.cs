79be90544e9b43b45bcff41d850112dab4f76906
38take <s.38take@gmail.com>
2013-10-22 21:45:10 -0700

    競合テスト

--- sample/Assets/PlayerScript.cs
+++ sample/Assets/PlayerScript.cs
@@ -6,24 +6,26 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		transform.position = new Vector3(0.0f,0.0f,0.0f);
+		transform.position = new Vector3(0.0f,2.0f,0.0f);
 		oldMouseX = Input.mousePosition.x;
+		rigidbody.AddForce(new Vector3(0.0f, 0.0f, 5.0f));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float transX = Input.mousePosition.x - oldMouseX;
+		float transX = (Input.mousePosition.x - oldMouseX) * 5.0f;
 		
-		transform.position = new Vector3(	Input.mousePosition.x,
-											transform.position.y,
-											transform.position.z + 0.1f);
-		
+		//transform.position = new Vector3(	transform.position.x + transX,
+		//									transform.position.y,
+		//									transform.position.z + 0.1f);
+		rigidbody.AddForce(new Vector3(transX, 0.0f, 5.0f));
 		if(transform.position.z >= 15.0f)
 		{
 			transform.position = new Vector3(	transform.position.x,
 												transform.position.y,
 												-15.0f);
 		}
+		oldMouseX = Input.mousePosition.x;
 	}
 	
 	private void OnCollisionEnter(Collision collision)
