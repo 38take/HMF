efca68a1dd24b1b13ca53e0645c6bc410aa136a4
Rayfol <mototake0211@gmail.com>
2013-10-27 07:29:44 -0700

    ・ナイフの仮オブジェクト表示 ・簡易的なTPｓ視点(今後改善) ・刃の簡易的な制御(今後改善) ・オブジェクトを消した時、スコアを加算

--- sample/Assets/Script/PlayerScript.cs
+++ sample/Assets/Script/PlayerScript.cs
@@ -2,13 +2,19 @@
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
-	float oldMouseX;
+	
+	ScoreScript				SScore;
+	private float			oldMouseX;
+	private float			Speed = 7.5f;
+	private bool			InitFall = false;
+	private	Vector3			oldPos;
 	
 	// Use this for initialization
 	void Start () {
-		transform.position = new Vector3(0.0f,2.0f,0.0f);
+		SScore = (ScoreScript)GameObject.Find("ScoreTextBox").GetComponent("ScoreScript");
+			
+		transform.position = new Vector3(0.0f,10.0f,0.0f);
 		oldMouseX = Input.mousePosition.x;
-		rigidbody.AddForce(new Vector3(0.0f, 0.0f, 5.0f));
 	}
 	
 	// Update is called once per frame
@@ -18,19 +24,45 @@ public class PlayerScript : MonoBehaviour {
 		//transform.position = new Vector3(	transform.position.x + transX,
 		//									transform.position.y,
 		//									transform.position.z + 0.1f);
-		rigidbody.AddForce(new Vector3(transX, 0.0f, 5.0f));
+		
+//		rigidbody.AddForce(new Vector3(transX, 0.0f, Speed));
+		
+		if( InitFall )
+			rigidbody.velocity = new Vector3(transX, rigidbody.velocity.y, Speed);
+
+		// 落下制限
+		if( Physics.Raycast(transform.position,Vector3.down,5) )
+		{
+			oldPos = transform.position;
+			
+			if( !InitFall )
+				InitFall = true;
+		}
+		else
+		{
+			if( InitFall )
+			{
+				transform.position = oldPos;
+			}
+		}
+		
 		if(transform.position.z >= 15.0f)
 		{
 			transform.position = new Vector3(	transform.position.x,
 												transform.position.y,
 												-15.0f);
 		}
+		
 		oldMouseX = Input.mousePosition.x;
+	
 	}
 	
 	private void OnCollisionEnter(Collision collision)
 	{
 		if(collision.gameObject.name != "Floor")
+		{
+			SScore.AddScore(100);
 			Destroy(collision.gameObject);
+		}
 	}
 }
---
+++ sample/Assets/Script/ScoreScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+	
+	private	float Score = 0.0f;
+	// Use this for initialization
+	void Start () {
+		guiText.text = "スコア：" + Score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		guiText.text = "スコア：" + Score;
+	}
+	
+	public void AddScore( float scr )
+	{
+		Score += scr;
+	}
+	public void Test()
+	{
+		Debug.Log("てす");
+	}
+}
