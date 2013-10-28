2941c5cc073c46ee4d8f01122f6a050f976a8e2e
Rayfol <mototake0211@gmail.com>
2013-10-28 06:55:22 -0700

    ・左右移動を秒数制限で制御 ・ターゲットを切った時にエフェクト発生 ・FPSを設定（現在はPlayerScriptにて設定）

--- sample/Assets/Script/PlayerScript.cs
+++ sample/Assets/Script/PlayerScript.cs
@@ -4,33 +4,88 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour {
 	
 	ScoreScript				SScore;
-	private float			oldMouseX;
-	private float			Speed = 7.5f;
-	private bool			InitFall = false;
 	private	Vector3			oldPos;
+	private float			oldMouseX;
+	private float			Speed;
+	private float			RightTime,LeftTime;
+	private	float			TransX_Limit = 10;
+	private float			TransX_LimitTime = 5;
+//	private bool			InitFall = false;
+	
+	public	GameObject		obj_Particle;
 	
 	// Use this for initialization
 	void Start () {
-		SScore = (ScoreScript)GameObject.Find("ScoreTextBox").GetComponent("ScoreScript");
-			
+		// 他スクリプトの関数を参照可能にする
+		SScore = (ScoreScript)GameObject.Find("ScoreTextBox").GetComponent("ScoreScript");		
+		// FPSを60に設定
+		Application.targetFrameRate = 60;
+		// FPS * 制限移動秒数
+		RightTime = LeftTime = Application.targetFrameRate * TransX_LimitTime;
+		// 初期位置格納
 		transform.position = new Vector3(0.0f,10.0f,0.0f);
 		oldMouseX = Input.mousePosition.x;
+		Speed = 7.5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float transX = (Input.mousePosition.x - oldMouseX) * 5.0f;
+		float	transX = (Input.mousePosition.x - oldMouseX) * 1.0f;
+/*
+		float	transX = 0;
+		float	mouseX = Input.mousePosition.x - oldMouseX;
 		
-		//transform.position = new Vector3(	transform.position.x + transX,
-		//									transform.position.y,
-		//									transform.position.z + 0.1f);
+		if( Mathf.Abs(mouseX) <= TransX_Limit )
+		{
+			transX = mouseX * 2.0f;
+		}
+		else
+		{
+			if( Mathf.Sin(mouseX) < 0 )
+				transX =  TransX_Limit * 2.0f;
+			else
+				transX = -TransX_Limit * 2.0f;
+		}
+*/
+		// 右方向に移動する時
+		if( transX < 0 )
+		{
+			if( RightTime != 0 )
+			{
+				rigidbody.velocity =
+					new Vector3(transX, rigidbody.velocity.y, rigidbody.velocity.z);
+
+				RightTime	--;
+				LeftTime	++;
+			}
+		}
+		// 左方向に移動する時
+		else if( transX > 0 )
+		{
+			
+			if( LeftTime != 0 )
+			{
+				rigidbody.velocity =
+					new Vector3(transX, rigidbody.velocity.y, rigidbody.velocity.z);
+				
+				RightTime	++;
+				LeftTime	--;
+			}
+		}
 		
-//		rigidbody.AddForce(new Vector3(transX, 0.0f, Speed));
+		if( LeftTime == 0 || RightTime == 0 )
+			rigidbody.velocity =
+				new Vector3(0, rigidbody.velocity.y, Speed);
+		else
+			rigidbody.velocity =
+				new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, Speed);
+
 		
+/*		
 		if( InitFall )
 			rigidbody.velocity = new Vector3(transX, rigidbody.velocity.y, Speed);
 
-		// 落下制限
+		// 下方向にレイを飛ばす
 		if( Physics.Raycast(transform.position,Vector3.down,5) )
 		{
 			oldPos = transform.position;
@@ -45,12 +100,14 @@ public class PlayerScript : MonoBehaviour {
 				transform.position = oldPos;
 			}
 		}
+*/		
 		
 		if(transform.position.z >= 15.0f)
 		{
-			transform.position = new Vector3(	transform.position.x,
+			RightTime = LeftTime = Application.targetFrameRate * TransX_LimitTime;
+			transform.position = new Vector3(	0,
 												transform.position.y,
-												-15.0f);
+												-30.0f);
 		}
 		
 		oldMouseX = Input.mousePosition.x;
@@ -59,10 +116,16 @@ public class PlayerScript : MonoBehaviour {
 	
 	private void OnCollisionEnter(Collision collision)
 	{
-		if(collision.gameObject.name == "Target(Clone)")
+		if(collision.gameObject.name == "target(Clone)")
 		{
 			SScore.AddScore(100);
 			Destroy(collision.gameObject);
+			
+			GameObject ins_obj =
+				(GameObject)Instantiate(obj_Particle,
+										collision.gameObject.transform.position,
+										collision.gameObject.transform.rotation);
+			Destroy(ins_obj, 1.0f);
 		}
 	}
 }
