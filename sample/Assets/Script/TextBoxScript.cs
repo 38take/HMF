c14eb19f0a6cb5e03da8f4497cda56a64327a3f1
38take <s.38take@gmail.com>
2013-10-30 00:07:29 -0700

    アドベンチャーパートでの会話データの読み込み＋仮出力の実装

--- sample/Assets/Script/TextBoxScript.cs
+++ sample/Assets/Script/TextBoxScript.cs
@@ -2,12 +2,14 @@
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
 public class TextBoxScript : MonoBehaviour {
 	
 	int actID;
 	int numStatement;
 	int strIdx;
+	int charIdx;
 	int[] balloonArray;
 	string[] strArray;
 	string renderString;
@@ -21,11 +23,12 @@ public class TextBoxScript : MonoBehaviour {
 		//パラメータの初期化
 		numStatement 	= 0;
 		strIdx 			= 0;
+		charIdx			= 0;
 		initialized = false;
 		
-		Initialize();
+		initialized = Initialize();
 	}
-	void Initialize()
+	bool Initialize()
 	{
 		string[] strBase;
 		string[] split;
@@ -38,6 +41,7 @@ public class TextBoxScript : MonoBehaviour {
 		//会話IDを取得
         FileInfo fi = new FileInfo(Application.dataPath+"/GameData/act.txt");
         StreamReader sr = new StreamReader(fi.OpenRead());
+		actID = 0;
         while( sr.Peek() != -1 ){
 			tmp = sr.ReadLine();
 			strBase = tmp.Split(';');
@@ -50,14 +54,16 @@ public class TextBoxScript : MonoBehaviour {
         }
        	sr.Close();
 		//会話データを取得
+		char[] tmpCharArray;
         FileInfo fiData = new FileInfo(Application.dataPath+"/GameData/actData/"+actID+".txt");
-        StreamReader srData = new StreamReader(fiData.OpenRead());
+        StreamReader srData = new StreamReader(fiData.OpenRead(), Encoding.GetEncoding("Shift_JIS"));
         while( srData.Peek() != -1 ){
 			tmp = srData.ReadLine();
 			strBase = tmp.Split(';');
 			split = strBase[0].Split(',');
 			data.Add(int.Parse(split[0]));	//吹き出しID
 			data.Add(split[1]);				//文章
+			Debug.Log("log:"+split[1].ToString());
         }
        	srData.Close();
 		//会話データ格納
@@ -70,38 +76,42 @@ public class TextBoxScript : MonoBehaviour {
 			strArray[i] 	= data[i*2+1].ToString();
 		}
 		renderBase = strArray[0];//.ToString();
-		initialized = true;
+		return true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 		if(initialized)
 		{
-			int charIdx = 0;
 			if(strIdx < numStatement)
 			{
-				if(string.Compare(renderString, renderBase) == 0 && Input.GetKeyUp(KeyCode.Return))
+				if(string.Compare(renderString, renderBase) == 0)
 				{
-					strIdx++;
-					renderBase = strArray[strIdx].ToString();
-					charIdx = 0;
+					if(Input.GetKeyUp(KeyCode.Return))
+					{
+						renderString = "";
+						strIdx++;
+						if(strIdx < numStatement)
+							renderBase = strArray[strIdx].ToString();
+						charIdx = 0;
+					}
 				}
 				else
 				{
-					renderString = renderBase.Substring(0, charIdx);
-					charIdx++;
+					renderString += renderBase.Substring(charIdx, 1);
+					charIdx=charIdx+1;
 				}
 				
 				//テキスト表示
 				//GUIText gui = ((GameObject)GameObject.Find("TextBox")).GetComponent<GUIText>();
 				guiText.text = renderString;
+				//guiText.text = renderBase;
 			}
 		}
 		else
 		{
-			Initialize();
+			initialized = Initialize();
 		}
-		Debug.Log(numStatement+","+renderBase);
+		//Debug.Log(numStatement+","+renderBase);
 	}
 }
