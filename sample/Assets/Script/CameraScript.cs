9852d0b573cade6fdf4018bb738db2a59309e4e6
Rayfol <mototake0211@gmail.com>
2013-11-04 18:14:01 -0800

    ・PlayerScriptの使ってない変数の削除 ・ゲームスタート時のカメラの仮演出の追加

--- sample/Assets/Script/CameraScript.cs
+++ sample/Assets/Script/CameraScript.cs
@@ -6,24 +6,43 @@ public class CameraScript : MonoBehaviour {
 	LineManagerScript		SLineManager;
 	PlayerScript			SPlayerScript;
 	private	Vector3			TargetPos;
+	private	float			timer = 0;
 	
 	// Use this for initialization
 	void Start () {
 		SLineManager	= (LineManagerScript)GameObject.Find("LineManager").GetComponent("LineManagerScript");
 		SPlayerScript	= (PlayerScript)GameObject.Find("ナイフ5").GetComponent("PlayerScript");		
-		transform.position = new Vector3(0,10,0);
+		transform.position = new Vector3(0,1000,0);
+		transform.LookAt(GameObject.Find("Floor").transform.position);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		TargetPos = SLineManager.CalcPlayerPos(SPlayerScript.m_Timer, 0);
 		
-		// 現在の刃の位置から数秒前の位置にカメラをセットする
-		transform.position = SLineManager.CalcPlayerPos(SPlayerScript.m_Timer-60, 0);
-		transform.position = new Vector3(	transform.position.x,
-											transform.position.y+3.0f,
-											transform.position.z );
-		transform.LookAt(TargetPos);
+		if( SPlayerScript.m_CameraFlag )
+		{
+			TargetPos = SLineManager.CalcPlayerPos(SPlayerScript.m_Timer, 0);
+			
+			// 現在の刃の位置から数秒前の位置にカメラをセットする
+			transform.position = SLineManager.CalcPlayerPos(SPlayerScript.m_Timer-60, 0);
+			transform.position = new Vector3(	transform.position.x,
+												transform.position.y+3.0f,
+												transform.position.z );
+			transform.LookAt(TargetPos);
+		}
+		else
+		{
+			Vector3 start_pos	= new Vector3(1000,1000,0);
+			Vector3 end_pos		= new Vector3(0,3,0);
+			float	SLERP_TIME	= 180;
+
+			if( timer/SLERP_TIME < 1 )
+				// カメラ位置の球面線形
+				transform.position = Vector3.Slerp(	start_pos, end_pos, timer/SLERP_TIME );
+			else
+				SPlayerScript.m_CameraFlag = true;
+
+			timer++;
+		}
 	}
 }
--- sample/Assets/Script/PlayerScript.cs
+++ sample/Assets/Script/PlayerScript.cs
@@ -5,19 +5,14 @@ public class PlayerScript : MonoBehaviour {
 	
 	ScoreScript				SScore;
 	LineManagerScript		SLineManager;
-	private	Vector3			oldPos;
 	private float			oldMouseX;
-	private float			Speed;
-	private float			RightTime,LeftTime;
-	private	float			TransX_Limit = 10;
-	private float			TransX_LimitTime = 5;
 	private float			height;
 	private float			HEIGHT;
-	private int			holdHorizontal;	           
-//	private bool			InitFall = false;
-	
+	private int				holdHorizontal;
+
 	public	GameObject		obj_Particle;
 	
+	public	bool			m_CameraFlag = false;
 	public	float			m_Offset;
 	public	int				m_Timer;
  	
@@ -28,12 +23,10 @@ public class PlayerScript : MonoBehaviour {
 		SLineManager = (LineManagerScript)GameObject.Find("LineManager").GetComponent("LineManagerScript");		
 		// FPSを60に設定
 		Application.targetFrameRate = 60;
-		// FPS * 制限移動秒数
-		RightTime = LeftTime = Application.targetFrameRate * TransX_LimitTime;
 		// 初期位置格納
 		transform.position = new Vector3(0.0f,10.0f,0.0f);
 		oldMouseX = Input.mousePosition.x;
-		Speed = 7.5f;
+
 		HEIGHT = 8.0f;
 		height = 0.0f;
 		
@@ -45,43 +38,46 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float	transX = (Input.mousePosition.x - oldMouseX) * 0.01f;
-		m_Timer+=4;
+		if( m_CameraFlag )
+		{
+			float	transX = (Input.mousePosition.x - oldMouseX) * 0.01f;
+			m_Timer+=4;
+			
+			//------------------------------------//
+			//移動
+			if(holdHorizontal <= 0)
+				m_Offset += transX;
+			else
+				holdHorizontal--;
+			Debug.Log(holdHorizontal);
+			if(m_Offset > 1.0f) m_Offset = 1.0f;
+			if(m_Offset < -1.0f) m_Offset = -1.0f;
+			Vector3 basePos = SLineManager.CalcPlayerPos(m_Timer, m_Offset);
+			if(Input.GetMouseButton(0))
+				height += (HEIGHT-height)*0.1f;
+			else
+				height += (0.0f-height)*0.1f;
+			transform.position = new Vector3(basePos.x, basePos.y+height, basePos.z);
+			oldMouseX = Input.mousePosition.x;
+			//------------------------------------//
+			//回転処理
+			//向くべき方向を算出
+			int inLineID = SLineManager.GetPlyaerLineID();
+			Vector3 dir = SLineManager.GetLineDirection(inLineID+1)-SLineManager.GetLineDirection(inLineID);
+			dir = Vector3.Normalize(dir);
+			
+	        // モデルのデフォルト向きによって基準ベクトルは任意調整
+			Vector3 vecDefault = new Vector3(1.0f,0.0f,0.0f);
 		
-		//------------------------------------//
-		//移動
-		if(holdHorizontal <= 0)
-			m_Offset += transX;
-		else
-			holdHorizontal--;
-		Debug.Log(holdHorizontal);
-		if(m_Offset > 1.0f) m_Offset = 1.0f;
-		if(m_Offset < -1.0f) m_Offset = -1.0f;
-		Vector3 basePos = SLineManager.CalcPlayerPos(m_Timer, m_Offset);
-		if(Input.GetMouseButton(0))
-			height += (HEIGHT-height)*0.1f;
-		else
-			height += (0.0f-height)*0.1f;
-		transform.position = new Vector3(basePos.x, basePos.y+height, basePos.z);
-		oldMouseX = Input.mousePosition.x;
-		//------------------------------------//
-		//回転処理
-		//向くべき方向を算出
-		int inLineID = SLineManager.GetPlyaerLineID();
-		Vector3 dir = SLineManager.GetLineDirection(inLineID+1)-SLineManager.GetLineDirection(inLineID);
-		dir = Vector3.Normalize(dir);
-		
-        // モデルのデフォルト向きによって基準ベクトルは任意調整
-		Vector3 vecDefault = new Vector3(1.0f,0.0f,0.0f);
-	
-	        // 0~360の値が欲しいので２倍
-		float rad = Mathf.Atan2(dir.x-vecDefault.x,dir.z-vecDefault.z)*2.0f;
-		float deg = rad * Mathf.Rad2Deg -180.0f;
-		
-	    //一度角度をリセットしてから回転する
-		transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
-	    // X軸基準に回す
-		transform.Rotate(310.0f, deg,0);
+		        // 0~360の値が欲しいので２倍
+			float rad = Mathf.Atan2(dir.x-vecDefault.x,dir.z-vecDefault.z)*2.0f;
+			float deg = rad * Mathf.Rad2Deg -180.0f;
+			
+		    //一度角度をリセットしてから回転する
+			transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
+		    // X軸基準に回す
+			transform.Rotate(310.0f, deg,0);
+		}
 	}
 	
 	private void OnCollisionEnter(Collision collision)
