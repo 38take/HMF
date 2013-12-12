a740f8aa42dc2bc43029f8773addee68bb962c37
38take <s.38take@gmail.com>
2013-12-12 01:01:24 -0800

    プレイリザルト　アチーブメントの表示

--- sample/Assets/Script/ResultRendererScript.cs
+++ sample/Assets/Script/ResultRendererScript.cs
@@ -21,12 +21,15 @@ public class ResultRendererScript : MonoBehaviour {
 	int upCnt;
 	int numJudgeKind;
 	int[] numJudge;
+	bool deadLine;
 	
 	//各種画像へのアクセス用
 	Tex2DGUITextureScript SResultLogo;
 	Tex2DGUITextureScript SScoreLogo;
 	Tex2DGUITextureScript SComboLogo;
 	Tex2DGUITextureScript SRank;
+	public GameObject obj_Achievement;
+	ArrayList AchievementArray;
 	
 	//外部オブジェクト
 	public GameObject	obj_ScoreRenderer;
@@ -36,6 +39,12 @@ public class ResultRendererScript : MonoBehaviour {
 	private bool end;
 	private bool dispScore = false;
 	private bool dispCombo = false;
+	private int  achievementIdx;
+	private int  achievementDispSpan;
+	private int  ACHIEVEMENT_DISP_SPAN = 10;
+	
+	//各項目の表示位置（インスペクタで編集できるように）
+	public Vector2 posAchievement;
 	
 	// Use this for initialization
 	void Start () {
@@ -50,6 +59,8 @@ public class ResultRendererScript : MonoBehaviour {
 		SScoreLogo 	= ((GameObject)GameObject.Find("ResultScore")).GetComponent<Tex2DGUITextureScript>();
 		SComboLogo 	= ((GameObject)GameObject.Find("ResultCombo")).GetComponent<Tex2DGUITextureScript>();
 		SRank 		= ((GameObject)GameObject.Find("ResultRank")).GetComponent<Tex2DGUITextureScript>();
+		//アチーブメントの表示
+		AchievementArray = new ArrayList();
 	}
 	// Update is called once per frame
 	void Update () {
@@ -150,6 +161,22 @@ public class ResultRendererScript : MonoBehaviour {
 	//アチーブメントボーナス表示
 	bool ActAchievement()
 	{
+		Vector2 work;
+		if(achievementDispSpan > 0)
+			achievementDispSpan--;
+		else
+		{
+			if(achievementIdx < AchievementArray.Count)
+				achievementIdx++;
+			achievementDispSpan = ACHIEVEMENT_DISP_SPAN;
+		}
+		for(int i=0; i<achievementIdx; i++)
+		{
+			work = ((Tex2DGUITextureScript)AchievementArray[i]).GetPos();
+			((Tex2DGUITextureScript)AchievementArray[i]).SetRenderFlag(true);
+			((Tex2DGUITextureScript)AchievementArray[i]).SetPos(work.x+((posAchievement.x - work.x)*0.1f), 
+																work.y, false);
+		}
 		if(Input.GetMouseButtonDown(0))
 			return false;
 		return true;
@@ -182,11 +209,82 @@ public class ResultRendererScript : MonoBehaviour {
 		bomb = SPlayer.GetNumBomb();
 		//持ち上げた回数
 		upCnt = SPlayer.GetNumUp();
+		//ラインアウトしていないか
+		deadLine = false;
 		//各判定の数
 		for(int i=0; i<numJudgeKind; i++)
 			numJudge[i] = SLineManager.GetNumJudge(i);
 		//クリティカルコンボ数
 		criticalCombo = SLineManager.GetNumCriticalCombo();
+		
+		//アチーブメントの判定
+		CheckAchievement();
+		achievementIdx = 0;
+	}
+	//アチーブメントの判定
+	void CheckAchievement()
+	{
+		//判定用計算
+		int numCut = numJudge[(int)LineManagerScript.JUDGE.GOOD] +
+			         numJudge[(int)LineManagerScript.JUDGE.NORMAL] +
+			         numJudge[(int)LineManagerScript.JUDGE.SAFE] ;
+			
+		//配列クリア
+		AchievementArray.Clear();
+		//順番に判定していく
+		if(numJudge[(int)LineManagerScript.JUDGE.MISS] == 0)	AddAchievement(0);
+		
+		if(	numCut == 0)	AddAchievement(1);
+	
+		if(!deadLine)		AddAchievement(2);
+		
+		if(bomb > 6)		AddAchievement(3);
+		else if(bomb > 4)	AddAchievement(4);
+		else if(bomb > 2)	AddAchievement(5);
+		
+		if(		combo > 100)	AddAchievement(6);
+		else if(combo > 80) 	AddAchievement(7);		
+		else if(combo > 60) 	AddAchievement(8);
+		else if(combo > 40) 	AddAchievement(9);
+		else if(combo > 20) 	AddAchievement(10);
+		
+		if(		numCut > 100)	AddAchievement(11);
+		else if(numCut > 80) 	AddAchievement(12);		
+		else if(numCut > 50) 	AddAchievement(13);
+		else if(numCut > 30) 	AddAchievement(14);
+		else if(numCut > 10) 	AddAchievement(15);
+		
+		if(	 		criticalCombo> 100)	AddAchievement(16);
+		else  if(  	criticalCombo> 80) 	AddAchievement(17);	
+		else  if(  	criticalCombo> 50) 	AddAchievement(18);
+		else  if(  	criticalCombo> 30) 	AddAchievement(19);
+		else  if(  	criticalCombo> 10) 	AddAchievement(20);
+		
+		if(	 	  numJudge[(int)LineManagerScript.JUDGE.GOOD] > 100)	AddAchievement(21);
+		else  if( numJudge[(int)LineManagerScript.JUDGE.GOOD] > 80) 	AddAchievement(22);		
+		else  if( numJudge[(int)LineManagerScript.JUDGE.GOOD] > 60) 	AddAchievement(23);
+		else  if( numJudge[(int)LineManagerScript.JUDGE.GOOD] > 40) 	AddAchievement(24);
+		else  if( numJudge[(int)LineManagerScript.JUDGE.GOOD] > 20) 	AddAchievement(25);
+		else  if( numJudge[(int)LineManagerScript.JUDGE.GOOD] > 10) 	AddAchievement(26);
+		
+		if(		upCnt >= 30 )	AddAchievement(29);
+		else if(upCnt >= 15)	AddAchievement(28);
+		else if(upCnt >= 10)	AddAchievement(27);
+		
+		//複合ボーナス
+		if(criticalCombo> 100 && numJudge[(int)LineManagerScript.JUDGE.GOOD] > 100) AddAchievement(30);
+		if(combo > 100 && numJudge[(int)LineManagerScript.JUDGE.GOOD] > 100)		AddAchievement(31);
+		if(numJudge[(int)LineManagerScript.JUDGE.MISS]==0 && !deadLine && bomb>6)	AddAchievement(32);
+		if(!deadLine && upCnt >= 15)												AddAchievement(33);
+	}
+	void AddAchievement(int id)
+	{
+		int idx = AchievementArray.Count;
+		GameObject obj = (GameObject)Instantiate(obj_Achievement, new Vector3(0.0f, 0.0f, 0.0f), new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
+		AchievementArray.Add(obj.GetComponent<Tex2DGUITextureScript>());
+		((Tex2DGUITextureScript)AchievementArray[idx]).SetPos(posAchievement.x + 300, posAchievement.y+(70*idx), true);
+		((Tex2DGUITextureScript)AchievementArray[idx]).SwitchTexture(id);
+		((Tex2DGUITextureScript)AchievementArray[idx]).SetRenderFlag(false);
 	}
 	
 	public void Validate()
